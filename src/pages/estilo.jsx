376f197fb1b96ac59eaec5c3e37c6d7d5498da09
Francisco barros <franciscobarros754@gmail.com>
2021-10-12 23:29:37 -0300

    Css module

---
+++ src/pages/estilo.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/estilo.module.css';
+
+export default function Estilo() {
+  return (
+    <div className={styles.container}>
+      <h1>Meu componente</h1>
+    </div>
+  )
+}
