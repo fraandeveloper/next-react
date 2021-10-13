bb030d8ed5a07ffc8bf91d592032121af00729fe
Francisco barros <franciscobarros754@gmail.com>
2021-10-13 00:06:09 -0300

    Navegacao de rotas

--- src/pages/estilo.jsx
+++ src/pages/estilo.jsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import styles from '../styles/estilo.module.css';
 
 import Header from '../components/Header';
@@ -5,6 +7,7 @@ import Header from '../components/Header';
 export default function Estilo() {
   return (
     <div className={styles.container}>
+      <Link href="/">Voltar</Link>
       <h1>Meu componente</h1>
       <Header title="Header" subTitle="paragrafo"/>
     </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,12 @@
+import Link from 'next/link'
+
 export default function App() {
   const [a,b] = [1,4];
   const header = <h1>Component como funcao</h1>
 
   return (
     <div>
+      <Link href="/estilo">Rota estiloso</Link>
       { header }
       <p>{ a + b }</p>
     </div>
